4bc15c98557412e7af692c582d5857904dff65fe
bokumoku <jati.hiliamsyah@gmail.com>
2022-05-18 23:51:04 +0900

    Initial commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Shouter shouter = new Shouter();
+		
+		shouter.shout("hurrah");
+	}
+
+}
---
+++ src/main/java/Shouter.java
@@ -0,0 +1,12 @@
+
+public class Shouter {
+
+	public void shout(String message) {
+		
+		String shoutMessage = message.toUpperCase() + '!';
+		
+		System.out.println(shoutMessage);
+		
+	}
+	
+}
